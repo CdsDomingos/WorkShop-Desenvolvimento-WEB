93f38324c6c3c58e5397ad7d941861a6f6e54f54
Cristiano Domingos <cristiano.domingos@dcx.ufpb.br>
2025-10-19 18:30:29 -0300

    Adicionada entidade Tarefa

---
+++ workshopdevweb/src/main/java/br/com/cristiano/workshopdevweb/domain/Tarefa.java
@@ -0,0 +1,84 @@
+package br.com.cristiano.workshopdevweb.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private LocalDateTime dataDeCriacao;
+
+    @Column(nullable = false)
+    private LocalDate dataDeEntrega;
+
+    @Column(nullable = true)
+    private LocalDate dataFinal;
+
+    public Tarefa() {
+        this.dataDeCriacao = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(LocalDate dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+
+    public LocalDate getDataDeEntrega() {
+        return dataDeEntrega;
+    }
+
+    public void setDataDeEntrega(LocalDate dataDeEntrega) {
+        this.dataDeEntrega = dataDeEntrega;
+    }
+
+    public LocalDateTime getDataDeCriacao() {
+        return dataDeCriacao;
+    }
+
+    public void setDataDeCriacao(LocalDateTime dataDeCriacao) {
+        this.dataDeCriacao = dataDeCriacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Tarefa tarefa = (Tarefa) o;
+        return Objects.equals(id, tarefa.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
