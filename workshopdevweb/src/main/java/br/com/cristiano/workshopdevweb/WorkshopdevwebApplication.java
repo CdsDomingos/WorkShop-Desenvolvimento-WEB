9cd5c789ddd0297d6c2141c38aa7a4a03e42ecfc
Cristiano Domingos <cristiano.domingos@dcx.ufpb.br>
2025-10-19 16:11:44 -0300

    Estrutura inicial do nosso Projeto

---
+++ workshopdevweb/src/main/java/br/com/cristiano/workshopdevweb/WorkshopdevwebApplication.java
@@ -0,0 +1,13 @@
+package br.com.cristiano.workshopdevweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopdevwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopdevwebApplication.class, args);
+	}
+
+}
---
+++ workshopdevweb/src/test/java/br/com/cristiano/workshopdevweb/WorkshopdevwebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cristiano.workshopdevweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopdevwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
